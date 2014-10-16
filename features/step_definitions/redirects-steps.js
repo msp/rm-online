893ed6bd2dd87749dc4827501d771b7e43c24253
msp <matt@cenatus.org>
2014-10-16 16:37:02 +0100

    Redirects: tighten Cukes.

--- features/step_definitions/redirects-steps.js
+++ features/step_definitions/redirects-steps.js
@@ -1,13 +1,19 @@
 var myStepDefinitionsWrapper = function () {
+  
+  var WAIT = 500
+  
   this.When(/^I visit (.*) from the old site$/, function (oldURL,callback) {
     console.log("Visiting: "+oldURL);
     this.visit(oldURL, callback);
   });
 
   this.Then(/^I should be redirected to (.*)$/, function (newURL, callback) {
-//    this.assert.equal(this.browser.statusCode, 301);
-    this.assert.equal(this.browser.location.pathname, newURL);
-    callback();
+    self = this;
+    this.browser.wait(WAIT, function() {
+//    self.assert.equal(self.browser.statusCode, 301);
+      self.assert.equal(self.browser.location.pathname, newURL);
+      callback();
+    })
   });
 };
 module.exports = myStepDefinitionsWrapper;
